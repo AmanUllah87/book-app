29e8e8b47a7c56c13e50d8262073a93b93c52764
AS <asif26.baust@gmail.com>
2026-04-10 15:51:19 +0600

    Initial Html and CSS

---
+++ src/app/catalog/catalog.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'book-catalog',
+  imports: [],
+  templateUrl: './catalog.html',
+  styleUrl: './catalog.scss',
+})
+export class Catalog {}
