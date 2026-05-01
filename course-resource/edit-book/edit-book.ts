4afc6e4c1d4c60ad52d3d50f29eb90d131bbe910
AS <181935390+asif-parvez-sarker@users.noreply.github.com>
2026-05-01 18:04:54 +0600

    Resource For Today Class

---
+++ course-resource/edit-book/edit-book.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-edit-book',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './edit-book.html',
+  styleUrl: './edit-book.scss',
+})
+export class EditBook implements OnInit {
+  constructor(private route: ActivatedRoute) {}
+
+  ngOnInit() {
+    this.route.paramMap.subscribe(paramMap => {
+      const bookId = paramMap.get('id');
+      if (!bookId) return;
+    });
+  }
+
+  saveBook() {}
+}
